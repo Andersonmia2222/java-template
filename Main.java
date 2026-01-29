a0211b57df336c52ab536777651cb409f5ea6afc
Andersonmia2222 <mia.anderson2026@gbwl.org>
2026-01-29 19:26:12 +0000

    4.3

--- Main.java
+++ Main.java
@@ -1,6 +1,25 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
-   }
-}
+     
+
+      
+
+// 10 array is a block of memory that stores a collection
+//of data items(elements) of the same type
+// under one variable name 
+
+
+// how to declear an array variable 
+String[] apCompSci = new String[13];
+double[] prices = new double[5]; 
+String[] names = new String[5];
+int[] luckyNums = new int[10];
+boolean[] yesNo = new boolean[3];//default values false 
+
+// how to access a value in an array 
+System.out.println(prices[0] );
+
+   }//ends main Method
+
+}// end main class 
