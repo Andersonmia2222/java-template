f0f18f702ddcdf0e612949bb5d6377b0365fa408
Andersonmia2222 <mia.anderson2026@gbwl.org>
2026-02-03 15:55:35 +0000

    updated

--- Main.java
+++ Main.java
@@ -45,6 +45,36 @@ System.out.println( table[ finalIndex]); // table[2] = "Aaniyah"
 System.out.println(table[ table/length -1 ]); // same as above 
 
 
+// *** 4.4 TRAVERSING ARRAYS***
+double[] scores = {45.0, 20.0, 89.5, 97.8};
+// look at one item using an index 
+System.out.println( score[1] );
+// use a STANDARD FOR LOOP to iterate through EVERY item 
+
+for ( int i = 0; i < 4; i++ ){
+   System.out.println("Current item:" + scores[i] ); 
+
+   for ( int i =0; i< scores.length; i++) {
+      System.out.println("Current item:" + scores[i] );
+      // "curve the test scores" = modify array items
+scores[i] = scores[i] + 10;
+System.out.println("Score after curve:" + scores[i]); 
+}
+
+// SHORTCUT if you only want to visit items, not modify
+// use an enhanced for each loop
+double sum = 0;
+//for each individual score in the array of scores 
+for ( double score : scores ) {
+// dont need [], index is not relevent here 
+sum += score; // add current item to the sum 
+
+}
+ System.out.println(sum); 
+ double avg = sum / scores.length;
+ System.out.println(avg);
+ 
+
 
 
    }//ends main Method
